2f5f62435a1ef2809ede15cea6bc94ac9296346b
Dieter De Schrijver <dieterdeschrijver001@hotmail.com>
2020-12-27 15:21:37 +0100

    zzz

--- TravelClient/Services/UserService.cs
+++ TravelClient/Services/UserService.cs
@@ -8,11 +8,18 @@ namespace TravelClient.Services
 {
     public class UserService
     {
-        public async void SaveUser(String user)
+        public async void SaveUser(string user)
         {
             Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.TemporaryFolder;
             Windows.Storage.StorageFile currentUser = await storageFolder.GetFileAsync("currentUser");
             await Windows.Storage.FileIO.WriteTextAsync(currentUser, user);
+        }
+
+        public async Task<string> GetUser()
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.TemporaryFolder;
+            Windows.Storage.StorageFile currentUser = await storageFolder.GetFileAsync("currentUser");
+            return await Windows.Storage.FileIO.ReadTextAsync(currentUser);
         }
     }
 }
