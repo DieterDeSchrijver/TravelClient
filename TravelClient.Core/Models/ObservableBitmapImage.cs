ad79158253992d0f257f9ac52e469852c37d678c
DESKTOP-J4K6VP4\tijld <tijl.deblander@student.hogent.be>
2020-12-27 16:06:08 +0100

    Datacontext KING

---
+++ TravelClient.Core/Models/ObservableBitmapImage.cs
@@ -0,0 +1,19 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace TravelClient.Core.Models
+{
+    public class ObservableBitmapImage : ObservableObject
+    {
+        private BitmapImage _value;
+
+        public BitmapImage Value
+        {
+            get { return _value; }
+            set { Set("Value", ref _value, value); }
+        }
+    }
+}
--- TravelClient/ViewModels/NewListViewModel.cs
+++ TravelClient/ViewModels/NewListViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using GalaSoft.MvvmLight.Command;
+using Prism.Commands;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +12,21 @@ using TravelClient.Core.Models.requests;
 using TravelClient.Core.Services;
 using TravelClient.Services;
 using TravelClient.Views;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace TravelClient.ViewModels
 {
     public class NewListViewModel
     {
         private string _autoSuggestionBoxText;
-        public String[] Suggestions { get; set; } = new string[0];
+        public ObservableCollection<String> Suggestions { get; set; } = new ObservableCollection<string>();
         public NewListRequest NewListRequest { get; set; } = new NewListRequest();
+        public ObservableBitmapImage Image { get; set; } = new ObservableBitmapImage();
 
         List<LocObj> locs = new List<LocObj>();
         HttpDataService http = new HttpDataService();
-        LocObj chosenLoc;
-        string chosenLocImage;
+    
         public string AutoSuggestionBoxText
         {
             get { return _autoSuggestionBoxText; }
@@ -39,55 +43,56 @@ namespace TravelClient.ViewModels
         public ObservableString LocationSuggestBox { get; set; } = new ObservableString();
 
         public ICommand AddList;
+        public ICommand Query;
 
         public NewListViewModel()
         {
             AddList = new RelayCommand(CreateList, true);
+            Query = new DelegateCommand<AutoSuggestBoxQuerySubmittedEventArgs>(LocationAutoSuggest_QuerySubmitted);
         }
         public void LocationAutoSuggestTextChanged()
         {
                 if (this._autoSuggestionBoxText.Length > 1)
                 {
-                    Suggestions = this.GetSuggestionsAsync(this._autoSuggestionBoxText);
+                    this.GetSuggestionsAsync(this._autoSuggestionBoxText);
                 }
                 else
                 {
-                    Suggestions = new string[] { "No suggestions..." };
+                Suggestions.Clear();
+                Suggestions.Add("No suggestions...");
                 }
            
         }
 
-        private string[] GetSuggestionsAsync(string text)
+        private void GetSuggestionsAsync(string text)
         {
             Task t = Task.Run(async () =>
             {
                 locs = await http.GetLocationAsync($"https://api.mapbox.com/geocoding/v5/mapbox.places/{text}.json?access_token=pk.eyJ1IjoiZGlldGVyZHMiLCJhIjoiY2tpMGRlMTVmMDBvMjMwa2JveWYwY3k3eSJ9.I9Y9bm0oMnQJyshyZTKMdQ&autocomplete=true&limit=5");
             });
             t.Wait();
-
-            List<string> s = new List<string>();
+            Suggestions.Clear();
             foreach (var item in locs)
             {
-                s.Add(item.LocationNameLong);
+                Suggestions.Add(item.LocationNameLong);
             }
 
-            return s.ToArray();
         }
 
-   /*     public async void LocationAutoSuggest_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs arg)
+        public async void LocationAutoSuggest_QuerySubmitted(AutoSuggestBoxQuerySubmittedEventArgs arg)
         {
             if (arg.ChosenSuggestion != null && arg.ChosenSuggestion.ToString() != "No suggestions...")
             {
                 LocationSuggestBox.Value = arg.ChosenSuggestion.ToString();
-                chosenLoc = locs.Find(l => l.LocationNameLong == arg.ChosenSuggestion.ToString());
-                chosenLocImage = await http.GetImageAsync($"https://api.unsplash.com/search/photos?query={chosenLoc.LocationNameShort}&client_id=TiZc8-TmzwTTAO3vLdLw4PNX6nzwtFnF7UKotCLsumU&per_page=1");
-                image.Source = new BitmapImage(new Uri(chosenLocImage)); ;
+                LocObj location = locs.Find(l => l.LocationNameLong == arg.ChosenSuggestion.ToString());
+                var chosenLocImage = await http.GetImageAsync($"https://api.unsplash.com/search/photos?query={location.LocationNameShort}&client_id=TiZc8-TmzwTTAO3vLdLw4PNX6nzwtFnF7UKotCLsumU&per_page=1");
+                Image.Value = new BitmapImage(new Uri(chosenLocImage)); 
             }
             else
             {
-                LocationSuggestBox.Value = sender.Text;
+                //LocationSuggestBox.Value = sender.Text;
             }
-        }*/
+        }
 
         private async void CreateList()
         {
