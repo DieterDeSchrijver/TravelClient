5120504770f9445a6bfb41b691b1274633473f82
DESKTOP-J4K6VP4\tijld <tijl.deblander@student.hogent.be>
2020-12-27 15:06:06 +0100

    DOE RUSTIG OF OP UW BAKKES OKLM

--- TravelClient.Core/Models/requests/NewListRequest.cs
+++ TravelClient.Core/Models/requests/NewListRequest.cs
@@ -1,19 +1,66 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TravelClient.Core.Models.requests
 {
-    public class NewListRequest
+    public class NewListRequest : ObservableObject
     {
-        public string Listname { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-        public string Description { get; set; }
-        public string Name { get; set; }
-        public float LngCoord { get; set; }
-        public float LatCoord { get; set; }
-        public string Image { get; set; }
+        private string _listName;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private string _description;
+        private string _name;
+        private float _lngCoord;
+        private float _latCoord;
+        private string _image;
+        public string Listname 
+        {
+            get { return _listName; }
+            set { Set("ListName", ref _listName, value); }
+           }
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { Set("StartDate", ref _startDate, value); }
+        }
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set { Set("EndDate", ref _endDate, value); }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { Set("Description", ref _description, value); }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { Set("Name", ref _name, value); }
+        }
+        public float LngCoord
+        {
+            get { return _lngCoord; }
+            set { Set("LngCoord", ref _lngCoord, value); }
+        }
+        public float LatCoord
+        {
+            get { return _latCoord; }
+            set { Set("LatCoord", ref _latCoord, value); }
+        }
+        public string Image
+        {
+            get { return _image; }
+            set { Set("Image", ref _image, value); }
+        }
+
+        public NewListRequest()
+        {
+            this.StartDate = DateTime.Now;
+            this.EndDate = DateTime.Now;
+        }
 
         public NewListRequest(string listname, DateTime startDate, DateTime endDate, string description, string name, float lngCoord, float latCoord, string image)
         {
--- TravelClient/Views/NewList.xaml.cs
+++ TravelClient/Views/NewList.xaml.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using TravelClient.Core.Models.requests;
 using TravelClient.Core.Services;
 using TravelClient.Services;
+using TravelClient.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Services.Maps;
@@ -28,95 +29,15 @@ namespace TravelClient.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class NewList : Page, INotifyPropertyChanged
+    public sealed partial class NewList : Page
     {
-        List<LocObj> locs = new List<LocObj>();
-        HttpDataService http = new HttpDataService();
-        LocObj chosenLoc;
-        string chosenLocImage;
-        string s;
-
-        public event PropertyChangedEventHandler PropertyChanged;
+        public NewListViewModel ViewModel { get; set; } = new NewListViewModel();
 
         public NewList()
         {
             this.InitializeComponent();
 
-            Task task = Task.Run(async () =>
-            {
-                Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.TemporaryFolder;
-                Windows.Storage.StorageFile currentUser = await storageFolder.GetFileAsync("currentUser");
-                s = await Windows.Storage.FileIO.ReadTextAsync(currentUser);
-            });
-            task.Wait(); // Wait
         }
 
-        public void LocationAutoSuggest_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-        {
-            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
-            {
-                if (sender.Text.Length > 1)
-                {
-                    sender.ItemsSource = this.GetSuggestionsAsync(sender.Text);
-                }
-                else
-                {
-                    sender.ItemsSource = new string[] { "No suggestions..." };
-                }
-            }
-        }
-
-        private void Set<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (Equals(storage, value))
-            {
-                return;
-            }
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-        }
-
-        private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-
-        private string[] GetSuggestionsAsync(string text)
-        {
-            Task t = Task.Run(async () =>
-            {
-                locs = await http.GetLocationAsync($"https://api.mapbox.com/geocoding/v5/mapbox.places/{text}.json?access_token=pk.eyJ1IjoiZGlldGVyZHMiLCJhIjoiY2tpMGRlMTVmMDBvMjMwa2JveWYwY3k3eSJ9.I9Y9bm0oMnQJyshyZTKMdQ&autocomplete=true&limit=5");
-            });
-            t.Wait();
-
-            List<string> s = new List<string>();
-            foreach (var item in locs)
-            {
-                s.Add(item.LocationNameLong);
-            }
-            
-            return s.ToArray();
-        }
-
-        public async void LocationAutoSuggest_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs arg)
-        {
-            if (arg.ChosenSuggestion != null && arg.ChosenSuggestion.ToString() != "No suggestions...")
-            {
-                LocationSuggestBox.Text = arg.ChosenSuggestion.ToString();
-                chosenLoc = locs.Find(l => l.LocationNameLong == arg.ChosenSuggestion.ToString());
-                chosenLocImage = await http.GetImageAsync($"https://api.unsplash.com/search/photos?query={chosenLoc.LocationNameShort}&client_id=TiZc8-TmzwTTAO3vLdLw4PNX6nzwtFnF7UKotCLsumU&per_page=1");
-                image.Source = new BitmapImage(new Uri(chosenLocImage)); ;
-            }
-            else
-            {
-                LocationSuggestBox.Text = sender.Text;
-            }
-        }
-
-        private async void CreateButton_Click(object sender, RoutedEventArgs e)
-        {
-            NewListRequest request = new NewListRequest(listNameInput.Text, startDate.Date.Value.DateTime, endDate.Date.Value.DateTime, descriptionInput.Text, chosenLoc.LocationNameShort,chosenLoc.Lon, chosenLoc.Lat, chosenLocImage);
-            string response = await http.PostAsJsonAsync("http://localhost:5000/api/TravelList/CreateTravelList", request, s);
-            NavigationService.Navigate<TravelListDetail>(response);
-        }
     }
 }
