d0ab7ed9b632f27c833443cc2e8232378b04d744
Dieter De Schrijver <dieterdeschrijver001@hotmail.com>
2020-12-27 12:35:19 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ TravelClient/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelClient.Services
+{
+    public class UserService
+    {
+        public async void SaveUser(String user)
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.TemporaryFolder;
+            Windows.Storage.StorageFile currentUser = await storageFolder.GetFileAsync("currentUser");
+            await Windows.Storage.FileIO.WriteTextAsync(currentUser, user);
+        }
+    }
+}
