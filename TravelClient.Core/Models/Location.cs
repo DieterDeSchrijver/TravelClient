6172ebec5d1a7a01ad5185dc389ffc9b3f1310ee
DESKTOP-J4K6VP4\tijld <tijl.deblander@student.hogent.be>
2021-01-17 15:46:04 +0100

    srzejfêfe

--- TravelClient.Core/Models/Location.cs
+++ TravelClient.Core/Models/Location.cs
@@ -12,10 +12,10 @@ namespace TravelClient.Core.Models
         public float LngCoord { get; set; }
         public float LatCoord { get; set; }
         public DateTime LocalTime { get; set; }
-        public string ImageURL { get; set; }
+        public string Image { get; set; }
         public BitmapImage ImageSource {
              get {
-                var x = ImageURL ?? "https://unsplash.com/photos/okz_PbR7yl4w";
+                var x = Image ?? "https://unsplash.com/photos/okz_PbR7yl4w";
                 return new BitmapImage(new Uri(x));
             }
         }
@@ -27,7 +27,7 @@ namespace TravelClient.Core.Models
             Name = name;
             LngCoord = lngCoord;
             LatCoord = latCoord;
-            ImageURL = image;
+            Image = image;
         }
     }
 }
